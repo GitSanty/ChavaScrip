c2231897f37f3fb3e62ce0eea51cad76f499b546
GitSanty <sastetec@hotmail.com>
2017-06-13 10:04:55 +0200

    Add files via upload

---
+++ 01-VARIABLES/app.js
@@ -0,0 +1,20 @@
+// Langues
+var Lang = "fr";
+
+var Message;
+
+if ( Lang === "fr")  {
+
+    Message = "Bonjour tout le monde";
+}
+
+if ( Lang === "en")  {
+
+    Message = "Hello world";
+}
+
+if ( Lang === "es")  {
+
+    Message = "Hola, Mundo";
+}
+
