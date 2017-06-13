b29ed6ddea9dd257ebfddf949c88b3f21cb29fd8
GitSanty <sastetec@hotmail.com>
2017-06-13 10:07:40 +0200

    Add files via upload

---
+++ 03-FONCTIONS/app.js
@@ -0,0 +1,46 @@
+// Fonction qui fait des soustractions
+
+var soustr = function Soustractions(a,b){
+
+    return a - b;
+}
+
+var resultat_soust = soustr(5,7);
+
+
+// Function Division
+
+var div = function Division(a,b){
+
+    return a/b;
+}
+
+var resultat_div = div(45,60);
+
+//Fonction qui fait des multiplications
+
+var mul = function multiplication(a,b){
+    return a*b;
+}
+
+var resultat_multi = mul(4,6);
+
+
+
+//Fonction qui fait des calcule de pourcentages
+
+var porcent = function procentage (val,por) {
+
+    return (val*(por/100));
+
+}
+var resultat_porcent = porcent(100,50);
+
+//Faire un fonction qui fait des calcules de vitesse
+
+var vitesse = function (distance,temps) {
+
+    return (distance/temps) ;
+}
+
+var resultat_vitesse = vitesse(700,4) + "Km/h";
